fe23fed3c7ba633bae66371f9a8ff4373c3c7867
taehyounpark <taehyounpark@icloud.com>
2023-09-18 12:04:24 +0900

    misc

--- cmake/standalone.cmake
+++ cmake/standalone.cmake
@@ -13,9 +13,9 @@ add_library( AnalysisPlugins SHARED ${AnalysisPluginsSources} ${AnalysisPluginsH
 set(ANALOGICAL_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/analogical/include/)
 target_include_directories(AnalysisPlugins PUBLIC ${CMAKE_CURRENT_SOURCE_DIR} ${ANALOGICAL_INCLUDE_DIR})
 
-target_compile_features(AnalysisPlugins PRIVATE cxx_std_17)
+target_compile_features(AnalysisPlugins PUBLIC cxx_std_17)
 
-find_package( ROOT REQUIRED COMPONENTS Core Imt RIO Net Hist Graf Graf3d Gpad ROOTVecOps Tree TreePlayer Rint Postscript Matrix Physics MathCore Thread MultiProc ROOTDataFrame )
+find_package(ROOT REQUIRED COMPONENTS Core Imt RIO Net Hist Graf Graf3d Gpad ROOTVecOps Tree TreePlayer Rint Postscript Matrix Physics MathCore Thread MultiProc ROOTDataFrame )
 find_library(ROOT_TREEPLAYER_LIBRARY TreePlayer HINTS ${ROOT_LIBRARY_DIR} REQUIRED)
 find_library(ROOT_RDATAFRAME_LIBRARY ROOTDataFrame HINTS ${ROOT_LIBRARY_DIR} REQUIRED)
 target_link_libraries(AnalysisPlugins analogical ROOT::Core ROOT::RIO ROOT::Hist ROOT::Tree ROOT::TreePlayer ROOT::Imt ROOT::ROOTVecOps ROOT::ROOTDataFrame ROOT::Physics)
@@ -43,7 +43,7 @@ install(DIRECTORY
   )
 
 add_executable(tree_example examples/tree_example.cxx)
-target_compile_features(tree_example PRIVATE cxx_std_17)
+target_compile_features(tree_example PUBLIC cxx_std_17)
 target_include_directories(tree_example PUBLIC ${CMAKE_CURRENT_SOURCE_DIR} ${ANALOGICAL_INCLUDE_DIR} ${ROOT_INCLUDE_DIR} ${ROOT_LIBRARY_DIR})
 target_link_libraries(tree_example AnalysisPlugins ${ROOT_LIBRARIES})
 
