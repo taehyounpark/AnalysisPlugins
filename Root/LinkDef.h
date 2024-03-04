d144ce4a4920cb3ce855c5206056614d8187b1ce
taehyounpark <taehyounpark@icloud.com>
2024-03-03 22:19:07 -0500

    Upstream

--- Root/LinkDef.h
+++ Root/LinkDef.h
@@ -13,22 +13,6 @@
 
 #pragma link C++ class queryosity::dataflow + ;
 
-#pragma link C++ class queryosity::multithread + ;
-#pragma link C++ class queryosity::action + ;
-
-#pragma link C++ class queryosityterm < float> + ;
-#pragma link C++ class queryositycell < float> + ;
-#pragma link C++ class queryosity::column::observable < float> + ;
-#pragma link C++ class queryosityvariable < float> + ;
-
-#pragma link C++ class queryosity::selection + ;
-#pragma link C++ class queryosity::selection::cut + ;
-#pragma link C++ class queryosity::selection::weight + ;
-#pragma link C++ class queryosity::selection::cutflow + ;
-
-#pragma link C++ class queryosity::query + ;
-#pragma link C++ class queryosity::query::experiment + ;
-
 #pragma link C++ class Hist < 1, float> + ;
 #pragma link C++ class Hist < 2, float> + ;
 #pragma link C++ class Hist < 3, float> + ;
